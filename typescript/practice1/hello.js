70a55fde401656495d81ce498e7535d567553ef2
muthwanzim <91889890+muthwanzim@users.noreply.github.com>
2022-06-28 17:28:37 +0900

    Add files via upload

---
+++ typescript/practice1/hello.js
@@ -0,0 +1,5 @@
+function greeting(person) {
+    return "Hello, " + person;
+}
+var user = "Kim Coding";
+document.body.textContent = greeting(user);
---
+++ typescript/practice1/hello.ts
@@ -0,0 +1,8 @@
+
+function greeting(person: string) {
+    return "Hello, " + person
+}
+
+let user = "Kim Coding"
+
+document.body.textContent = greeting(user)
